1af06d1f56c6004905d2c70ca9693185b08c0c68
Yaroslav Ovdiienko <46504259+yaroslav-android@users.noreply.github.com>
2024-07-17 22:35:47 +0300

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/bt.h>
+
+&led_strip {
+    chain-length = <27>;
+};
